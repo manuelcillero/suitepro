b9e569d03fe8dfce5cfc2afc119d4083e9fc2406
Manuel Cillero <manuel@cillero.es>
2020-11-22 21:35:04 +0100

    Actualizar plugin Glossary a 1.1.0

--- plugins/redmine_glossary/config/routes.rb
+++ plugins/redmine_glossary/config/routes.rb
@@ -1,19 +1,17 @@
-RedmineApp::Application.routes.draw do
-    match 'glossary_styles/:action', :controller => :glossary_styles, :via => [ :get, :post, :put, :patch ]
+# Plugin's routes
+# See: http://guides.rubyonrails.org/routing.html
 
-    match 'projects/:project_id/glossary', :to => 'glossary#index', :via => [ :get ]
-    match 'projects/:project_id/glossary/new', :to => 'glossary#new', :via => [ :get ]
-    match 'projects/:project_id/glossary/edit', :to => 'glossary#edit', :via => [ :get, :post, :patch ]
-    match 'projects/:project_id/glossary/:id/edit', :to => 'glossary#edit', :id => /\d+/, :via => [ :get, :post, :patch ]
-    match 'projects/:project_id/glossary/:id/:action', :controller => :glossary, :id => /\d+/, :via => :all
-    match 'projects/:project_id/glossary/:id', :to => 'glossary#show', :id => /\d+/, :via => [ :get ]
-    match 'projects/:project_id/glossary/destroy', :to => 'glossary#destroy', :via => [ :delete ]
-    match 'projects/:project_id/glossary/:action', :controller => :glossary, :via => :all
-
-    match 'projects/:project_id/term_categories', :to => 'term_categories#index', :via => :all
-    match 'projects/:project_id/term_categories/destroy', :to => 'term_categories#destroy', :via => [ :delete ]
-    match 'projects/:project_id/term_categories/change_order', :to => 'term_categories#change_order', :via => [ :post ]
-    match 'projects/:project_id/term_categories/:action', :controller => :term_categories, :via => :all
-    match 'projects/:project_id/term_categories/:id/:action', :controller => :term_categories, :id => /\d+/, :via => :all
+Rails.application.routes.draw do
+  resources :projects do
+    resources :glossary_terms do
+      member do
+        patch 'preview'
+      end
+      collection do
+        post 'preview'
+        post 'import'
+      end
+    end
+    resources :glossary_categories
+  end
 end
-
