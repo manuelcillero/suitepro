0f87f1397f3d0246804b35279ccfcdaa6ca0d63e
Manuel Cillero <manuel@cillero.es>
2021-05-14 19:47:20 +0200

    Elimina defintivamente plugin Redmine Git Server

--- plugins/redmine_git_server/app/overrides/repositories/show.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new virtual_path: 'repositories/show',
-                     name: 'insert_clone_button',
-                     insert_bottom: 'div.contextual',
-                     partial: 'repositories/clone_button'
-
--- plugins/redmine_git_server/spec/helpers/git_server_helper_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe GitServerHelper, type: :helper do
-
-  it "should be tested (FIXME)" do
-  end
-
-end
